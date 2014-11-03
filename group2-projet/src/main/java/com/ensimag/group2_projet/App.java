985d4654269f7798f29022f67e834a1420b184bb
Andreiy <Yannick.Andrei@ensimag.grenoble-inp.fr>
2014-11-03 17:45:39 +0100

    add projet

---
+++ group2-projet/src/main/java/com/ensimag/group2_projet/App.java
@@ -0,0 +1,16 @@
+package com.ensimag.group2_projet;
+
+import com.ensimag.api.node.INode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
