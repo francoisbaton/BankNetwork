0c5cc1b35ad89fb560c0549f652ca5b4bd5e71cf
Clément Linarès <clementlinares@wificampus-029115.grenet.fr>
2014-11-14 14:53:23 +0100

    class abstract

--- group2-projet/src/main/java/com/ensimag/group2_projet/Server/Implem/BankActionImplemCloseAccount.java
+++ group2-projet/src/main/java/com/ensimag/group2_projet/Server/Implem/BankActionImplemCloseAccount.java
@@ -1,6 +1,9 @@
 package com.ensimag.group2_projet.Server.Implem;
 
+import java.io.Serializable;
 import java.rmi.RemoteException;
+
+import com.ensimag.api.bank.IBankNode;
 
 public class BankActionImplemCloseAccount extends BankActionImplem {
 
@@ -9,10 +12,15 @@ public class BankActionImplemCloseAccount extends BankActionImplem {
 	 * 
 	 */
 	private static final long serialVersionUID = 5542925532428218146L;
+	private long number;
 	
 	public BankActionImplemCloseAccount() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
+		this.number = 0;
+	}
+	
+	public BankActionImplemCloseAccount(long number) throws RemoteException {
+		this.number = number;
 	}
 
 
